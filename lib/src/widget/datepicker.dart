2f3fd4542eb071f660fca6e9974496935b9306fc
Obebe <shkim000905@naver.com>
2023-10-21 16:23:01 +0900

    데이트피커수정

--- lib/src/widget/datepicker.dart
+++ lib/src/widget/datepicker.dart
@@ -24,46 +24,77 @@ class _DatePickerState extends State<DatePicker> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Text('$_selectedYear년 $_selectedMonth월'),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: Text(
+              '$_selectedYear년 $_selectedMonth월',
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              _buildPickerItem(_selectedYear - 1, '년'),
-              _buildPickerItem(_selectedMonth - 1, '월'),
-              _buildPickerItem(_selectedYear + 1, '년'),
-              _buildPickerItem(_selectedMonth + 1, '월'),
+              _buildPickerItem(_selectedYear - 1, color: Colors.grey),
+              _buildPickerItem(_selectedMonth - 1, color: Colors.grey),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildPickerItem(_selectedYear, isBold: true, fontSize: 18.0),
+              _buildPickerItem(_selectedMonth, isBold: true, fontSize: 18.0),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildPickerItem(_selectedYear + 1, color: Colors.grey),
+              _buildPickerItem(_selectedMonth + 1, color: Colors.grey),
             ],
           ),
           ElevatedButton(
             onPressed: _onDateChanged,
-            child: Text('확인'),
+            child: Text(
+              '완료',
+              style: TextStyle(color: Colors.black),
+            ),
+            style: ElevatedButton.styleFrom(
+              elevation: 0.0,
+              primary: Colors.white,
+            ),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildPickerItem(int value, String label) {
-    return GestureDetector(
+  Widget _buildPickerItem(int value,
+      {bool isBold = false, double fontSize = 14.0, Color? color}) {
+    return InkWell(
       onTap: () {
-        if (label == '년') {
-          setState(() {
-            _selectedYear = value;
-          });
-        } else {
+        if (value <= 12) {
           setState(() {
             _selectedMonth = value;
           });
+        } else {
+          setState(() {
+            _selectedYear = value;
+          });
         }
       },
-      child: Column(
-        children: [
-          Text(value.toString()),
-          Text(label),
-        ],
+      child: Text(
+        value.toString(),
+        style: TextStyle(
+          fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+          fontSize: fontSize,
+          color: color,
+        ),
       ),
     );
   }
@@ -71,6 +102,6 @@ class _DatePickerState extends State<DatePicker> {
   void _onDateChanged() {
     DateTime selectedDate = DateTime(_selectedYear, _selectedMonth);
     widget.onDateChanged(selectedDate);
-    Navigator.of(context).pop(); // DateTime picker dialog를 닫습니다.
+    Navigator.of(context).pop();
   }
 }
