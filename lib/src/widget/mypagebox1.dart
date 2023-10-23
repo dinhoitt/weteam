8d4f5a12b9302bf262f3ff6df4608162ab408bd9
Obebe <shkim000905@naver.com>
2023-10-23 20:14:42 +0900

    툴팁 수정 완료

--- lib/src/widget/mypagebox1.dart
+++ lib/src/widget/mypagebox1.dart
@@ -4,12 +4,88 @@ import 'package:get/get.dart';
 import 'package:weteam/src/controller/mypage_controller.dart';
 import 'package:weteam/src/data/image_date.dart';
 
-class MyPageBox1 extends StatelessWidget {
+class MyPageBox1 extends StatefulWidget {
+  @override
+  _MyPageBox1State createState() => _MyPageBox1State();
+}
+
+class _MyPageBox1State extends State<MyPageBox1> {
   final MyPageController controller = Get.put(MyPageController());
   final double progressValue = 0.7; // 게이지 값
-  final GlobalKey _toolTipKey = GlobalKey(); // 툴팁 터치키
+  final GlobalKey _imageKey = GlobalKey(); // 팀메이트력 이미지 위치 찾아오는 Key
+  OverlayEntry? _overlayEntry; // 툴팁 대신하여 만든 오버레이
 
-  MyPageBox1({super.key});
+  void _showOverlay(BuildContext context) {
+    final RenderBox renderBox =
+        _imageKey.currentContext!.findRenderObject() as RenderBox;
+    final imagePosition = renderBox.localToGlobal(Offset.zero);
+    final imageSize = renderBox.size;
+
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Stack(
+        children: [
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: _removeOverlay,
+              child: Container(
+                color: Colors.black.withOpacity(0.3),
+              ),
+            ),
+          ),
+          Positioned(
+            top: imagePosition.dy + imageSize.height, // 툴팁 위치
+            child: Material(
+              color: Colors.transparent,
+              child: Container(
+                width: Get.width - 20.0,
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                padding: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0),
+                      child: Text(
+                        '팀메이트력이란?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '이전 팀플 팀원들의 평가를 기반으로 집계된 평점으로, 나의 팀플 매너를 수치로 나타냅니다.',
+                      style: TextStyle(),
+                      maxLines: 3, // 최대 줄 설정
+                      overflow: TextOverflow.ellipsis, // 글자가 넘칠 때 생략기호 표시
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void _removeOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -128,33 +204,13 @@ class MyPageBox1 extends StatelessWidget {
                     ),
                     child: InkWell(
                       onTap: () {
-                        final dynamic tooltip = _toolTipKey.currentState;
-                        tooltip?.ensureTooltipVisible();
+                        _showOverlay(context);
                       },
-                      child: Tooltip(
-                        key: _toolTipKey,
-                        message:
-                            '팀 메이트력이란? \n 이전 팀플 팀원들의 평가를 기반으로 집계된 평점으로, 나의 팀플 매너를 수치로 나타냅니다.',
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(4.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 1,
-                              blurRadius: 5,
-                              offset: Offset(0, 3),
-                            ),
-                          ],
-                        ),
-                        textStyle: TextStyle(
-                          color: Colors.black,
-                        ),
-                        child: Image.asset(
-                          ImagePath.miniinfo,
-                          width: 16,
-                          height: 16,
-                        ),
+                      child: Image.asset(
+                        key: _imageKey,
+                        ImagePath.miniinfo,
+                        width: 16,
+                        height: 16,
                       ),
                     ),
                   )
