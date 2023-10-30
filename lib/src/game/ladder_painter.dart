8b66347ee5458b05ed342ea2b8c19e71d823e079
obebe <shkim000905@naver.com>
2023-10-30 21:26:42 +0900

    사다리 가로선 추가

--- lib/src/game/ladder_painter.dart
+++ lib/src/game/ladder_painter.dart
@@ -5,9 +5,30 @@ class LadderPainter extends CustomPainter {
   final int itemCount;
   final double itemWidth;
   final Random rand = Random();
-  final List<double> horizontalLines = [];
+  final List<List<double>> horizontalLines = [];
 
-  LadderPainter({required this.itemWidth, required this.itemCount});
+  LadderPainter({required this.itemWidth, required this.itemCount}) {
+    while (horizontalLines.length < itemCount - 1) {
+      horizontalLines.add(_generateRandomHorizontalLines(horizontalLines));
+    }
+  }
+
+  List<double> _generateRandomHorizontalLines(
+      List<List<double>> existingLines) {
+    List<double> lines = [];
+    int numOfHorizontalLines = rand.nextInt(3) + 1; // 최대 3개의 수평선
+
+    for (int i = 0; i < numOfHorizontalLines; i++) {
+      double randomHeight;
+      do {
+        randomHeight = rand.nextDouble();
+      } while (lines.any((height) => (height - randomHeight).abs() < 0.1) ||
+          existingLines.any((list) => list.contains(randomHeight)));
+      lines.add(randomHeight);
+    }
+
+    return lines;
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -15,33 +36,27 @@ class LadderPainter extends CustomPainter {
       ..color = Colors.black
       ..strokeWidth = 2.0;
 
-    double startHeight = size.height * 0.05; // 사다리 시작
-    double endHeight = size.height * 0.95; // 사다리 끝
+    double startHeight = size.height * 0.05;
+    double endHeight = size.height * 0.95;
     double horizontalStartLimit = startHeight + (endHeight - startHeight) * 0.1;
     double horizontalEndLimit = endHeight - (endHeight - startHeight) * 0.1;
+
     for (int i = 0; i < itemCount; i++) {
       double startX = (i + 0.5) * itemWidth;
-
-      double randomHeight;
-      if (horizontalLines.length > i) {
-        randomHeight = horizontalLines[i]; // 저장된 값을 사용하여 사다리를 그림
-      } else {
-        randomHeight = horizontalStartLimit +
-            rand.nextDouble() * (horizontalEndLimit - horizontalStartLimit);
-        if (i < itemCount - 1) {
-          horizontalLines.add(randomHeight); // 새로 결정된 수평선의 위치를 리스트에 추가
-        }
-      }
 
       // 수직선 그리기
       canvas.drawLine(
           Offset(startX, startHeight), Offset(startX, endHeight), paint);
 
-      // 수평선(다리) 그리기
       if (i < itemCount - 1) {
         double nextX = (i + 1.5) * itemWidth; // 다음 아이템의 x 좌표
-        canvas.drawLine(
-            Offset(startX, randomHeight), Offset(nextX, randomHeight), paint);
+        for (var ratio in horizontalLines[i]) {
+          double actualHeight = horizontalStartLimit +
+              ratio * (horizontalEndLimit - horizontalStartLimit);
+          // 수평선(다리) 그리기
+          canvas.drawLine(
+              Offset(startX, actualHeight), Offset(nextX, actualHeight), paint);
+        }
       }
     }
   }
