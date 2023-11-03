d5dee753118e86315807c496fd4bafa724db74f1
Obebe <shkim000905@naver.com>
2023-11-04 05:20:04 +0900

    provider 삭제

--- lib/src/page/login/signup.dart
+++ lib/src/page/login/signup.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:weteam/src/data/image_date.dart';
 
-class SignUP extends StatelessWidget {
+class SignUP extends StatefulWidget {
   const SignUP({super.key});
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUP> {
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+  bool _isPasswordMatched = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _passwordController.addListener(_validatePassword);
+    _confirmPasswordController.addListener(_validatePassword);
+  }
+
+  _validatePassword() {
+    if (_passwordController.text == _confirmPasswordController.text) {
+      setState(() {
+        _isPasswordMatched = true;
+      });
+    } else {
+      setState(() {
+        _isPasswordMatched = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +72,7 @@ class SignUP extends StatelessWidget {
                           contentPadding: EdgeInsets.symmetric(
                               horizontal: 20.0, vertical: 10.0),
                           hintText: '아이디 입력(5~11자)',
+                          hintStyle: TextStyle(fontSize: 12.0),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(15.0),
                             borderSide:
@@ -51,23 +81,128 @@ class SignUP extends StatelessWidget {
                         ),
                       ),
                     ),
-                  ],
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 40.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      // 중복확인 버튼
-                    },
-                    child: Image.asset(
-                      ImagePath.signupcheck,
-                      height: 28.0,
-                      width: 75.0,
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            // 중복확인 버튼
+                          },
+                          child: Image.asset(
+                            ImagePath.signupcheck,
+                            height: 28.0,
+                            width: 75.0,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                    Text(
+                      '비밀번호',
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                      child: TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 10.0),
+                          hintText: '비밀번호(숫자, 영문, 특수문자 조합 최소 8자)',
+                          hintStyle: TextStyle(fontSize: 12.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide:
+                                BorderSide(width: 6.0, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      '비밀번호 확인',
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                      child: TextFormField(
+                        controller: _confirmPasswordController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 10.0),
+                          hintText: '비밀번호(숫자, 영문, 특수문자 조합 최소 8자)',
+                          hintStyle: TextStyle(fontSize: 12.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide:
+                                BorderSide(width: 6.0, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Checkbox(
+                      activeColor: Color(0xFFC86148),
+                      value: _isPasswordMatched,
+                      onChanged: (bool? newValue) {},
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            // 중복확인 버튼
+                          },
+                          child: Image.asset(
+                            ImagePath.signupcheck,
+                            height: 28.0,
+                            width: 75.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '닉네임',
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 10.0),
+                          hintText: '닉네임 입력',
+                          hintStyle: TextStyle(fontSize: 12.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide:
+                                BorderSide(width: 6.0, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            // 중복확인 버튼
+                          },
+                          child: Image.asset(
+                            ImagePath.signupcheck,
+                            height: 28.0,
+                            width: 75.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -75,5 +210,12 @@ class SignUP extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
   }
 }
