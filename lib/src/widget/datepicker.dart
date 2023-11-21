98b738f280196340781b73c51dd0aa5fb3cecde7
Obebe <shkim000905@naver.com>
2023-11-22 08:37:18 +0900

    Refactor:(datepicker.dart)

--- lib/src/widget/datepicker.dart
+++ lib/src/widget/datepicker.dart
@@ -4,135 +4,113 @@ class DatePicker extends StatefulWidget {
   final Function(DateTime? selectedDate) onDateChanged;
   final DateTime initialDate;
 
-  DatePicker({required this.onDateChanged, required this.initialDate});
+  const DatePicker(
+      {required this.onDateChanged, required this.initialDate, Key? key})
+      : super(key: key);
 
   @override
   State<DatePicker> createState() => _DatePickerState();
 }
 
 class _DatePickerState extends State<DatePicker> {
-  late int _selectedYear;
-  late int _selectedMonth;
+  late DateTime _selectedDate;
 
   @override
   void initState() {
     super.initState();
-    _selectedYear = widget.initialDate.year;
-    _selectedMonth = widget.initialDate.month;
+    _selectedDate = widget.initialDate;
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      contentPadding: EdgeInsets.only(top: 5.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      contentPadding: const EdgeInsets.only(top: 5.0),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 10.0),
-            child: Text(
-              '$_selectedYear년 $_selectedMonth월',
-              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _buildPickerItem(_selectedYear - 1, color: Colors.grey),
-              _buildPickerItem(_selectedMonth - 1, color: Colors.grey),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _buildPickerItem(_selectedYear, isBold: true, fontSize: 18.0),
-              _buildPickerItem(_selectedMonth, isBold: true, fontSize: 18.0),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _buildPickerItem(_selectedYear + 1, color: Colors.grey),
-              _buildPickerItem(_selectedMonth + 1, color: Colors.grey),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pop(context, true);
-                    },
-                    child: Text(
-                      '취소',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    style: TextButton.styleFrom(
-                      elevation: 0.0,
-                      primary: Colors.white,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: _onDateChanged,
-                    child: Text(
-                      '완료',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    style: TextButton.styleFrom(
-                      elevation: 0.0,
-                      primary: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          buildDateDisplay(),
+          buildPickerRow(_selectedDate.year - 1, _selectedDate.month - 1),
+          buildPickerRow(_selectedDate.year, _selectedDate.month,
+              isBold: true, fontSize: 18.0),
+          buildPickerRow(_selectedDate.year + 1, _selectedDate.month + 1),
+          buildActionButtons(),
         ],
       ),
     );
   }
 
-  Widget _buildPickerItem(int value,
-      {bool isBold = false, double fontSize = 14.0, Color? color}) {
+  Widget buildDateDisplay() => Padding(
+        padding: const EdgeInsets.only(bottom: 10.0),
+        child: Text(
+          '${_selectedDate.year}년 ${_selectedDate.month}월',
+          style: const TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+        ),
+      );
+
+  Widget buildPickerRow(int yearValue, int monthValue,
+          {bool isBold = false, double fontSize = 14.0}) =>
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          buildPickerItem(yearValue,
+              isYear: true, isBold: isBold, fontSize: fontSize),
+          buildPickerItem(monthValue, isBold: isBold, fontSize: fontSize),
+        ],
+      );
+
+  Widget buildPickerItem(int value,
+      {bool isYear = false, bool isBold = false, double fontSize = 14.0}) {
     return InkWell(
-      onTap: () {
-        setState(() {
-          if (value == 0 || value == 13) {
-            _selectedMonth = (value == 13) ? 1 : 12;
-            _selectedYear += (value == 13) ? 1 : -1;
-          } else if (value > 0 && value <= 12) {
-            _selectedMonth = value;
-          } else {
-            _selectedYear = value;
-          }
-        });
-      },
+      onTap: () => updateSelectedDate(value, isYear: isYear),
       child: Text(
-        (_selectedMonth == 12 && value == 13)
-            ? '1'
-            : (_selectedMonth == 1 && value == 0)
-                ? '12'
-                : value.toString(),
+        value.toString(),
         style: TextStyle(
           fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
           fontSize: fontSize,
-          color: color,
+          color: isSelectable(value, isYear) ? Colors.black : Colors.grey,
         ),
       ),
     );
   }
 
+  bool isSelectable(int value, bool isYear) {
+    if (isYear) return value >= 1;
+    return value >= 1 && value <= 12;
+  }
+
+  void updateSelectedDate(int value, {required bool isYear}) {
+    setState(() {
+      if (isYear) {
+        _selectedDate = DateTime(value, _selectedDate.month);
+      } else {
+        _selectedDate = DateTime(_selectedDate.year, value);
+      }
+    });
+  }
+
+  Widget buildActionButtons() => Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            buildActionButton('취소', () => Navigator.pop(context, true)),
+            buildActionButton('완료', _onDateChanged),
+          ],
+        ),
+      );
+
+  Widget buildActionButton(String text, VoidCallback onPressed) => Expanded(
+        child: TextButton(
+          onPressed: onPressed,
+          style: TextButton.styleFrom(
+              foregroundColor: Colors.white, elevation: 0.0),
+          child: Text(text, style: const TextStyle(color: Colors.black)),
+        ),
+      );
+
   void _onDateChanged() {
-    DateTime selectedDate = DateTime(_selectedYear, _selectedMonth);
-    widget.onDateChanged(selectedDate);
+    widget.onDateChanged(_selectedDate);
     Navigator.of(context).pop();
   }
 }
