34a68e95e024adc6d48be6873b7248ba685a4f10
Obebe <shkim000905@naver.com>
2023-11-27 08:53:13 +0900

    Error:datepicker

--- lib/src/widget/datepicker.dart
+++ lib/src/widget/datepicker.dart
@@ -52,25 +52,56 @@ class _DatePickerState extends State<DatePicker> {
       );
 
   Widget buildPickerRow(int yearValue, int monthValue, int dayValue,
-          {bool isBold = false, double fontSize = 14.0}) =>
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          buildPickerItem(yearValue,
-              isYear: true, isBold: isBold, fontSize: fontSize),
-          buildPickerItem(monthValue, isBold: isBold, fontSize: fontSize),
-          buildPickerItem(dayValue, isBold: isBold, fontSize: fontSize)
-        ],
-      );
+      {bool isBold = false, double fontSize = 14.0}) {
+    // Calculate the last day of the selected month
+    int lastDayOfCurrentMonth = getLastDayOfMonth(yearValue, monthValue);
+
+    // Adjust the day values for wrapping
+    int nextDayValue = dayValue >= lastDayOfCurrentMonth ? 1 : dayValue + 1;
+    int nextMonthValue =
+        dayValue >= lastDayOfCurrentMonth ? monthValue % 12 + 1 : monthValue;
+    int nextYearValue = monthValue == 12 && dayValue >= lastDayOfCurrentMonth
+        ? yearValue + 1
+        : yearValue;
+
+    // Build the row with the year, month, and day values
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        buildPickerItem(yearValue,
+            isYear: true, isBold: isBold, fontSize: fontSize),
+        buildPickerItem(monthValue,
+            isMonth: true, isBold: isBold, fontSize: fontSize),
+        buildPickerItem(dayValue, isBold: isBold, fontSize: fontSize),
+        // If the day is the last day of the month, show the first day of the next month
+        if (dayValue == lastDayOfCurrentMonth)
+          buildPickerItem(nextDayValue,
+              isBold: isBold,
+              fontSize: fontSize,
+              isNextMonth: true,
+              nextMonthValue: nextMonthValue,
+              nextYearValue: nextYearValue),
+      ],
+    );
+  }
 
   Widget buildPickerItem(int value,
       {bool isYear = false,
       bool isMonth = false,
       bool isBold = false,
-      double fontSize = 14.0}) {
+      double fontSize = 14.0,
+      bool isNextMonth = false,
+      int nextMonthValue = 0,
+      int nextYearValue = 0}) {
     return InkWell(
       onTap: () {
-        if (isSelectable(value, isYear, isMonth)) {
+        if (isNextMonth) {
+          updateSelectedDate(1,
+              isYear: false,
+              isMonth: true,
+              monthValue: nextMonthValue,
+              yearValue: nextYearValue);
+        } else if (isSelectable(value, isYear, isMonth)) {
           updateSelectedDate(value, isYear: isYear, isMonth: isMonth);
         }
       },
@@ -79,8 +110,9 @@ class _DatePickerState extends State<DatePicker> {
         style: TextStyle(
           fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
           fontSize: fontSize,
-          color:
-              isSelectable(value, isYear, isMonth) ? Colors.black : Colors.grey,
+          color: isSelectable(value, isYear, isMonth) || isNextMonth
+              ? Colors.black
+              : Colors.grey,
         ),
       ),
     );
@@ -93,22 +125,48 @@ class _DatePickerState extends State<DatePicker> {
     } else if (isMonth) {
       return value >= 1 && value <= 12;
     } else {
-      // 마지막 날짜를 확인하여 일(day) 선택 가능 여부를 반환합니다.
       int lastDay = getLastDayOfMonth(_selectedDate.year, _selectedDate.month);
       return value >= 1 && value <= lastDay;
     }
   }
 
   void updateSelectedDate(int value,
-      {bool isYear = false, bool isMonth = false}) {
+      {required bool isYear,
+      required bool isMonth,
+      int? monthValue,
+      int? yearValue}) {
     setState(() {
+      int year = yearValue ?? _selectedDate.year;
+      int month = monthValue ?? _selectedDate.month;
+      int day = _selectedDate.day;
+
       if (isYear) {
-        _selectedDate = DateTime(value, _selectedDate.month, _selectedDate.day);
+        // Update the year value
+        _selectedDate = DateTime(value, month, day);
       } else if (isMonth) {
-        _selectedDate = DateTime(_selectedDate.year, value, _selectedDate.day);
+        // Update the month value and handle the day wrapping if necessary
+        if (day > getLastDayOfMonth(year, value)) {
+          day = 1; // Wrap to the first day of the next month
+          month += 1; // Increment the month
+          if (month > 12) {
+            month = 1; // Wrap to January
+            year += 1; // Increment the year
+          }
+        }
+        _selectedDate = DateTime(year, value, day);
       } else {
-        _selectedDate =
-            DateTime(_selectedDate.year, _selectedDate.month, value);
+        // Update the day value
+        if (value > getLastDayOfMonth(year, month)) {
+          day = 1; // Wrap to the first day of the next month
+          month += 1; // Increment the month
+          if (month > 12) {
+            month = 1; // Wrap to January
+            year += 1; // Increment the year
+          }
+        } else {
+          day = value; // Set day to selected value
+        }
+        _selectedDate = DateTime(year, month, day);
       }
     });
   }
